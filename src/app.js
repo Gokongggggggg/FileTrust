a575bffdb9fb9565572d41f8c1d95a119f3095ce
GoKonggg <wiselyjansonhalim90@gmail.com>
2026-03-15 12:08:11 +0700

    fix: wildcard route syntax for Express v5

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use('/api/result', resultRouter);
 
 // Serve React frontend
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
